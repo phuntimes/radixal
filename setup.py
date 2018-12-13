7a0aba2d07215ad6c43c7bb19bb67c8d72642b95
Sean McVeigh <spmcveigh@gmail.com>
2018-12-13 15:01:24 -0800

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 
-version = "0.1.5"
+version = "0.1.6"
 
 
 packages = find_packages(
--- src/radixal/errors.py
+++ src/radixal/errors.py
@@ -7,7 +7,7 @@ error messages.
 """
 
 __all__ = ['InvalidBase', 'UnknownCharacter']
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 __author__ = 'Sean McVeigh'
 
 
