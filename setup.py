6447d0544244d9a8b6f577f736bb8d32f40d4587
Sean McVeigh <spmcveigh@gmail.com>
2018-12-13 15:01:19 -0800

    Add 3.5 to classifiers

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ classifiers = [
     "Development Status :: 4 - Beta"
     "Intended Audience :: Developers"
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7"
     "License :: OSI Approved :: MIT License",
